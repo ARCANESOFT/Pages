f918a395d1c62e85ad3ec0e5a82e47cd453ee023
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-04-15 16:38:21 +0100

    Adding the sidebar items

--- config/sidebar/main.php
+++ config/sidebar/main.php
@@ -1,5 +1,26 @@
 <?php
 
 return [
-
+    'title'       => 'CMS',
+    'name'        => 'cms',
+    'icon'        => 'fa fa-fw fa-files-o',
+    'roles'       => [],
+    'permissions' => [],
+    'children'    => [
+        [
+            'title'       => 'Statistics',
+            'name'        => 'cms-dashboard',
+            'route'       => 'pages::foundation.dashboard',
+            'icon'        => 'fa fa-fw fa-bar-chart',
+            'roles'       => [],
+            'permissions' => ['pages.dashboard.stats'],
+        ],[
+            'title'       => 'Pages',
+            'name'        => 'cms-pages',
+            'route'       => 'pages::foundation.pages.index',
+            'icon'        => 'fa fa-fw fa-file-text-o',
+            'roles'       => [],
+            'permissions' => ['pages.posts.list'],
+        ]
+    ],
 ];
