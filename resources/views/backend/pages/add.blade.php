b5aca0c456164fad855a2ecb4492d864445f6178
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-04-15 16:37:03 +0100

    Adding views

---
+++ resources/views/backend/pages/add.blade.php
@@ -0,0 +1,9 @@
+@section('header')
+    <h1><i class="fa fa-fw fa-file-text-o"></i> Pages <small> - Add a new page</small></h1>
+@endsection
+
+@section('content')
+@endsection
+
+@section('scripts')
+@endsection
---
+++ resources/views/backend/pages/edit.blade.php
@@ -0,0 +1,9 @@
+@section('header')
+    <h1><i class="fa fa-fw fa-file-text-o"></i> Pages <small> - Edit the page</small></h1>
+@endsection
+
+@section('content')
+@endsection
+
+@section('scripts')
+@endsection
